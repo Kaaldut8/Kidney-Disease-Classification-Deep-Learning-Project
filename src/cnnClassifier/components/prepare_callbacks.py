e714d50b386c7bda5db0bffe933bcd658641a834
Kaaldut8 <raghavmaheshwari0803@gmail.com>
2026-09-07 19:40:58 +0530

    all notebooks added except evaluation

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -13,7 +13,7 @@ class PrepareCallbacks:
 
     @property
     def _create_tb_callbacks(self):
-        timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+        timestamp = time.strftime("%d-%m-%Y : %H:%M:%S")
         tb_running_log_dir = os.path.join(
             self.config.tensorboard_root_log_dir,
             f"tb_logs_at_{timestamp}"
